2ee262681e1d031dcbe245add50066f6546c491a
Julio Ucharima <julio_ucharima@usmp.pe>
2024-05-26 19:14:24 -0500

    add firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { firebaseProviders } from './firebase/firebase.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    firebaseProviders,
+  ],
 };
---
+++ src/app/firebase/firebase.config.ts
@@ -0,0 +1,12 @@
+import { EnvironmentProviders } from "@angular/core";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { environment } from "../../environments/environment";
+
+
+const firebaseProviders: EnvironmentProviders[] = ([
+  provideFirebaseApp(() => initializeApp(environment.firebase)),
+  provideFirestore(() => getFirestore()),
+]);
+
+export { firebaseProviders };
